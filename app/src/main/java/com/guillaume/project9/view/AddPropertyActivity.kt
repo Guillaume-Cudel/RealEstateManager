9807af62cf252703e022c3926238f526a8cf75a5
Guillaume-Cudel <58982293+Guillaume-Cudel@users.noreply.github.com>
2022-04-07 12:06:10 +0200

    Provide AddPropertyActivity view (less pick picture)

--- app/src/main/java/com/guillaume/project9/view/AddPropertyActivity.kt
+++ app/src/main/java/com/guillaume/project9/view/AddPropertyActivity.kt
@@ -2,11 +2,22 @@ package com.guillaume.project9.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
+import android.widget.TextView
 import com.guillaume.project9.R
 
 class AddPropertyActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_property)
+
+        val estateAgents = resources.getStringArray(R.array.Estate_agents)
+        val arrayAdapter = ArrayAdapter(this, R.layout.estate_agent_list_items, estateAgents)
+        val autoCompleteTextView: AutoCompleteTextView = findViewById(R.id.add_property_estate_agent_text)
+        autoCompleteTextView.setAdapter(arrayAdapter)
     }
+
+
 }
--- app/src/main/java/com/guillaume/project9/view/MainActivity.kt
+++ app/src/main/java/com/guillaume/project9/view/MainActivity.kt
@@ -1,12 +1,29 @@
 package com.guillaume.project9.view
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.guillaume.project9.R
 
 class MainActivity : AppCompatActivity() {
+
+    //private lateinit var binding: MainActivityBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        /*binding = MainActivityBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)*/
+
+        val bSwitchActivity: Button = findViewById<Button>(R.id.main_button)
+
+
+        bSwitchActivity.setOnClickListener {
+            //val intent: Intent = Intent(this, AddPropertyActivity)
+            startActivity(Intent(this, AddPropertyActivity::class.java))
+        }
+
     }
 }
